f19fad1a314da2485fae75d231e696840797ad88
ppanjwan <126329795+ppanjwan@users.noreply.github.com>
2023-03-01 14:00:42 +0530

    Update index.js

--- kalvium-file-manager/index.js
+++ kalvium-file-manager/index.js
@@ -4,32 +4,6 @@
 const fs = require('fs');
 const { logBlue, logGreen, logRed } = require('./logging.js');
 
-Object.defineProperty(global, '__stack', {
-  get: function () {
-    var orig = Error.prepareStackTrace;
-    Error.prepareStackTrace = function (_, stack) {
-      return stack;
-    };
-    var err = new Error();
-    Error.captureStackTrace(err, arguments.callee);
-    var stack = err.stack;
-    Error.prepareStackTrace = orig;
-    return stack;
-  },
-});
-
-Object.defineProperty(global, '__line', {
-  get: function () {
-    return __stack[1].getLineNumber();
-  },
-});
-
-Object.defineProperty(global, '__function', {
-  get: function () {
-    return __stack[1].getFunctionName();
-  },
-});
-
 var dataAsync = '';
 var dataSync = '';
 
@@ -175,18 +149,8 @@ function readAsyncWriteSync() {
 }
 
 //------------------------------------------------------------------------
-// Combinations
 readWriteSync();
 readSyncWriteAsync();
 readWriteAsync();
 readAsyncWriteSync();
-
 //------------------------------------------------------------------------
-// Combinations end here
-
-// //Individual calls
-// readFileAsync("./KFM-source/input2.txt");
-// dataSync = readFileSync("./KFM-source/input.txt");
-
-// writeFileSync("./KFM-destination/output.txt", dataSync);
-// writeFileAsync("./KFM-destination/output2.txt", dataAsync);console.log(`Hello Node.js v${process.versions.node}!`);
